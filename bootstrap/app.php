ed6df40a9b28991bb1ad3890ffe89f77d4ae56dd
Absar Ali <absar0306@gmail.com>
2024-07-29 15:18:23 +0500

    feat: add admin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
