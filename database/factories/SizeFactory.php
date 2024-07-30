8ae70773abca1889690abbac8e9860f0ab6af6b1
Ayeshasaqib <ayeshssaqib2@gmail.com>
2024-07-30 12:48:15 +0500

    Feat : Add Size model, controller, factory and seeder

---
+++ database/factories/SizeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Size>
+ */
+class SizeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_id'=>Product::all()->random()->id,
+            'title'=>fake()->title(),
+            'stock'=>fake()->numberBetween(1,10),
+        ];
+    }
+}
