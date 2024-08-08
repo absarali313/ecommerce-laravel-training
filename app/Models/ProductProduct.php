51e3bd1d88250afbc8b6c96ac2b93059d3812cfb
Absar Ali <absar@Absars-MacBook-Air.local>
2024-08-08 12:35:22 +0500

    refactor: add update or delete function in related products

--- app/Models/ProductProduct.php
+++ app/Models/ProductProduct.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Http\Request;
 
 class ProductProduct extends Model
 {
@@ -24,5 +25,27 @@ class ProductProduct extends Model
     {
         return $query->where('product_id', $product)
             ->where('related_product_id', $relatedProduct);
+    }
+
+    /**
+     * Update or delete the product
+     * @param Request $relationData
+     * @param Product $product
+     * @return void
+     */
+    public static function updateOrDelete(Request $relationData,Product $product)
+    {
+        $action = $relationData->input('action');
+        if ($action == 'update')
+        {
+            ProductProduct::where('product_id',$relationData->product_id)->where( 'related_product_id',$product->id)->update([
+                'product_id' => $relationData->product_id,
+                'related_product_id' => $relationData->related_id,
+            ]);
+        }
+        elseif ($action == 'delete')
+        {
+            ProductProduct::where('product_id',$relationData->product_id)->where( 'related_product_id',$product->id)->delete();
+        }
     }
 }
